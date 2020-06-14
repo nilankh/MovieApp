62e79fd7ddc2d35b10f30cb57bc1555a30805d94
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-14 16:15:18 +0530

    combining reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,18 @@
-import { ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES} from '../actions';
+import {
+    ADD_MOVIES, 
+    ADD_TO_FAVOURITES, 
+    REMOVE_FROM_FAVOURITES, 
+    SET_SHOW_FAVOURITES,
+    ADD_MOVIE_TO_LIST
+} from '../actions';
 
 const initialMovieState = {
     list: [],
     favourites: [],
     showFavourites: false
-}
-export default function movies (state = initialMovieState, action) {
+};
+export  function movies (state = initialMovieState, action) {
+    console.log('MOVIES REDUCER');
     // if(action.type === ADD_MOVIES){
     //     return {
     //         ...state,
@@ -38,8 +45,32 @@ export default function movies (state = initialMovieState, action) {
                 ...state,
                 showFavourites: action.val
             }
+        case ADD_MOVIE_TO_LIST:
+            return {
+                ...state,
+                list: [action.movie, ...state.list]
+            };
         default:
             return state;
     }
 }
+
+const initialSearchState = { 
+    result: {} 
+};
+export function search(state = initialSearchState, action) {
+    console.log('SEARCH REDUCER');
+    return state;
+}
+const initialRootState = {
+    movies: initialMovieState,
+    search: initialSearchState
+};
+export default function rootReducer(state = initialRootState, action) {
+    return {
+        movies: movies(state.movies, action),
+        search: search(state.search, action)
+    }
+}
+
 
