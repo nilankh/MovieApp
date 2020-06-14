d5f864e8c4838b1d8519ca14ea2fbf531263668b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-14 16:56:32 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+// function logger(obk, next, action)
+// logger(obj)(next)(action)
+const logger = function({ dispatch, getState }) {
+    return function(next) {
+        return function(action) {
+            // Middleware code
+            console.log('ACTION_TYPE = ', action.type);
+            next(action);
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store', store);
 // console.log('BEFORE STATE', store.getState());
 
